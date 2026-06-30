b1e2c10e56bfef9783f42cd8af93a2efed10dc7b
MohamedRabea <mohamedabuelroos27@gmail.com>
2026-06-30 10:50:16 +0300

    remove unecessary code

--- app/projects/layout/projects-grid.tsx
+++ app/projects/layout/projects-grid.tsx
@@ -1,5 +1,4 @@
 import { Project } from "@/types/project";
-import { useInView } from "@/hooks/use-in-view";
 import ProjectCard from "./project-card";
 
 interface Props {
@@ -7,13 +6,8 @@ interface Props {
 }
 
 export default function ProjectsGrid({ projects }: Props) {
-  const { ref, visible } = useInView();
-
   return (
-    <div
-      ref={ref}
-      className={`space-y-20  animate-fade-in`}
-    >
+    <div className={`space-y-20  animate-fade-in`}>
       {projects.map((project) => (
         <ProjectCard key={project.id} project={project} />
       ))}
